91f84c23d0035127eb5de0af07d78e6133c2bce2
Occidi <gwendal.jeanson@gmail.com>
2026-01-23 00:52:17 +0100

    number and math operators

--- learning.py
+++ learning.py
@@ -50,7 +50,7 @@ FIRST = "First"
 SECOND = "second"
 THIRD = 3
 print(FIRST + " " + SECOND) # First second
-print(FIRST + " " + SECOND + " " + THIRD ) # TypeError: can only concatenate str (not "int") to str
+# print(FIRST + " " + SECOND + " " + THIRD ) # TypeError: can only concatenate str (not "int") to str
 print(FIRST + " " + SECOND + " " + str(THIRD)) # First second 3
 
 #string literal aka f-strings (starts with f"")
@@ -81,5 +81,82 @@ print(SECOND.isupper()) # False
 print(SECOND.upper().isupper()) # True
 print(FIRST.islower()) # False
 print(FIRST.lower().islower()) # True
-newTitle = "the title"
-print(newTitle.title()) # The Title
+NEWTITLE = "the title"
+print(NEWTITLE.title()) # The Title
+
+
+# NUMBERS AND MATHS
+
+my_int = 10
+print(type(my_int)) # <class 'int'>
+my_neg_int = -5
+
+sum_ints = my_int + my_neg_int  # 5
+sub_ints = my_int - my_neg_int # 15
+multi_ints = my_int * my_neg_int # -50
+div_ints = my_int / my_neg_int # -2
+
+my_float = 12.0
+my_neg_float = -4.9
+
+print(type(my_float)) # <class 'float'>
+
+# operation with both ints and float result in float
+sum_int_and_float = my_int + my_float # 22.0
+print(type(sum_int_and_float)) # <class 'float'>
+
+mod_ints = my_int % my_neg_int # 0
+mod_floats = my_float % my_neg_float # -2.700000000000001
+
+#Floor division divides two numbers and returns the greatest integer >= to the result
+my_int_2 = 4
+floor_div_ints = my_int // my_int_2 # 2
+
+expo_int = my_int ** my_int_2 # 10000
+
+#built in function
+my_float_from_int = float(my_int)  # 10.0
+my_float_from_string = float("42.0") # 42.0
+print(type(my_float_from_int), type(my_float_from_string))  # <class 'float'> <class 'float'>
+
+my_float_2 = 12.92563
+my_int_from_float = int(my_float) # 12
+my_int_from_string = int("42") # 42
+print(type(my_int_from_float), type(my_int_from_string))  # <class 'int'> <class 'int'>
+
+rounded_int_1 = round(my_float_2) # 13
+rounded_int_2 = round(my_float_2, 1) # 12.9
+
+# abs returns the absolute value of a number,
+num = -15
+absolute_value = abs(num) # 15
+
+
+# pow raises a number to the power of another or performs modular exponentiation.
+result_1 = pow(2, 3)  # Equivalent to 2 ** 3 = 8
+result_2 = pow(2, 3, 5)  # (2 ** 3) % 5 = 3
+
+#assignement operators
+my_var = 10
+my_var += 5 # equivalent to my_var = my_var + 5 // 15
+my_var -= 3 # 12
+my_var *= 2 # 24
+my_var /= 2 # 12.0
+my_var //= 5 # 2.0
+my_var %= 2 # 0.0
+new_var = 10
+new_var **= 3 # 1000
+
+# can use + and * operators on strings (other throw TypeError)
+greet = 'Hello'
+greet += ' World'
+print(greet) # Hello World
+
+greet = 'Hello'
+greet *= 3
+print(greet) # HelloHelloHello
+
+# increment (there is no my_var++ or ++my_var)
+my_var = 5
+my_var += 1
+print(my_var) # 6
